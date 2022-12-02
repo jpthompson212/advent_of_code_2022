9650339e0b4268e07dbfcbea68d0bbdf04227fa2
Johnathan Thompson <jpthompson212@gmail.com>
2022-12-02 01:08:39 -0500

    After the fact dictionary cleanup

--- python/02/02.py
+++ python/02/02.py
@@ -61,3 +61,51 @@ with open('python/02/02_input.txt', 'r') as file:
         total_score += round_score
         
 print(f'Total Score is: {total_score}')
+
+# Clean up by using dictionaries
+
+# rock paper scisors
+values = {'X': 1,
+          'Y': 2,
+          'Z': 3}
+
+# lose draw win
+values_part2 = {'X': 0,
+                'Y': 3,
+                'Z': 6}
+
+result = {'AX': 3 + values['X'],
+          'AY': 6 + values['Y'],
+          'AZ': 0 + values['Z'],
+          'BX': 0 + values['X'],
+          'BY': 3 + values['Y'],
+          'BZ': 6 + values['Z'],
+          'CX': 6 + values['X'],
+          'CY': 0 + values['Y'],
+          'CZ': 3 + values['Z']}
+
+result_part2 = {'AX': 3 + values_part2['X'],
+                'AY': 1 + values_part2['Y'],
+                'AZ': 2 + values_part2['Z'],
+                'BX': 1 + values_part2['X'],
+                'BY': 2 + values_part2['Y'],
+                'BZ': 3 + values_part2['Z'],
+                'CX': 2 + values_part2['X'],
+                'CY': 3 + values_part2['Y'],
+                'CZ': 1 + values_part2['Z']}
+
+with open('python/02/02_input.txt', 'r') as file:
+    lines = file.readlines()
+    total_score = 0
+    # Part 1
+    for line in lines:
+        round = line.strip().replace(' ', '')
+        total_score += result[round]
+    print(f'Total Score is: {total_score}')
+    
+    total_score = 0
+    # Part 2
+    for line in lines:
+        round = line.strip().replace(' ', '')
+        total_score += result_part2[round]
+    print(f'Total Score is: {total_score}')
