0a7a11dbc012cc96027bd91cfcc94c81fa4db605
Johnathan Thompson <jpthompson212@gmail.com>
2022-12-05 02:40:02 -0500

    Actually read stacks

--- python/05/05.py
+++ python/05/05.py
@@ -1,26 +1,20 @@
-import pandas as pd
+import copy
 import re
 
-hard_code = {1: ['W','R','F'],
-             2: ['T','H','M','C','D','V','W','P'],
-             3: ['P','M','Z','N','L'],
-             4: ['J','C','H','R'],
-             5: ['C','P','G','H','Q','T','B'],
-             6: ['G','C','W','L','F','Z'],
-             7: ['W','V','L','Q','Z','J','G','C'],
-             8: ['P','N','R','F','W','T','V','C'],
-             9: ['J','W','H','G','R','S','V']}
-
-hard_code_2 = {1: ['W','R','F'],
-             2: ['T','H','M','C','D','V','W','P'],
-             3: ['P','M','Z','N','L'],
-             4: ['J','C','H','R'],
-             5: ['C','P','G','H','Q','T','B'],
-             6: ['G','C','W','L','F','Z'],
-             7: ['W','V','L','Q','Z','J','G','C'],
-             8: ['P','N','R','F','W','T','V','C'],
-             9: ['J','W','H','G','R','S','V']}
-
+with open('python/05/05_input_stack.txt', 'r') as file:
+    lines = file.readlines()
+    stacks = {}
+    length = len(lines.pop().split())
+    [stacks.setdefault(i + 1, []) for i in range(length)]
+    
+    for line in lines:
+        for i, val in enumerate(line[1::4]):
+            if val != ' ':
+                stacks[i+1].append(val)
+                
+    [stacks[i + 1].reverse() for i in range(length)]
+    stacks_2 = copy.deepcopy(stacks)
+                
 with open('python/05/05_input.txt', 'r') as file:
     lines = file.readlines()
     
@@ -30,13 +24,15 @@ with open('python/05/05_input.txt', 'r') as file:
         
         numbers = [int(s) for s in re.findall(r'\d+', line)]
         
-        # Part 1
+        # Part 1  
         for i in range(0, numbers[0]):
-            hard_code[numbers[2]].append(hard_code[numbers[1]].pop())
+            stacks[numbers[2]].append(stacks[numbers[1]].pop())
             
         # Part 2
-        hard_code_2[numbers[2]].extend(hard_code_2[numbers[1]][-numbers[0]:])
-        hard_code_2[numbers[1]] = hard_code_2[numbers[1]][:-numbers[0]]
+        stacks_2[numbers[2]].extend(stacks_2[numbers[1]][-numbers[0]:])
+        stacks_2[numbers[1]] = stacks_2[numbers[1]][:-numbers[0]]
 
-print(f'{hard_code[1][-1]}{hard_code[2][-1]}{hard_code[3][-1]}{hard_code[4][-1]}{hard_code[5][-1]}{hard_code[6][-1]}{hard_code[7][-1]}{hard_code[8][-1]}{hard_code[9][-1]}')
-print(f'{hard_code_2[1][-1]}{hard_code_2[2][-1]}{hard_code_2[3][-1]}{hard_code_2[4][-1]}{hard_code_2[5][-1]}{hard_code_2[6][-1]}{hard_code_2[7][-1]}{hard_code_2[8][-1]}{hard_code_2[9][-1]}')
+print(f'{stacks[1][-1]}{stacks[2][-1]}{stacks[3][-1]}{stacks[4][-1]}{stacks[5][-1]}\
+    {stacks[6][-1]}{stacks[7][-1]}{stacks[8][-1]}{stacks[9][-1]}')
+print(f'{stacks_2[1][-1]}{stacks_2[2][-1]}{stacks_2[3][-1]}{stacks_2[4][-1]}\
+    {stacks_2[5][-1]}{stacks_2[6][-1]}{stacks_2[7][-1]}{stacks_2[8][-1]}{stacks_2[9][-1]}')
