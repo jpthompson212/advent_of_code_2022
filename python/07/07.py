234deb62ab2d6b66f899878a376d32107f332533
jpthompson212 <jpthompson212@gmail.com>
2022-12-07 14:54:53 -0500

    Day 7

--- python/07/07.py
+++ python/07/07.py
@@ -1,15 +1,14 @@
 import pprint
-below_100000 = []
-accounted_for = []
+import flatdict
+
 def append_dict(dict, keys, values):
     for key in keys[:-1]:
         dict = dict.setdefault(key, {})
     dict[keys[-1]] = values
 
-
-with open('python/07/07_example.txt', 'r') as file:
+with open('python/07/07_input.txt', 'r') as file:
     lines = file.readlines()
-    
+
     directory_structure = {}
     init = False
     list_dir = False
@@ -23,14 +22,14 @@ with open('python/07/07_example.txt', 'r') as file:
                         init = True
                     else:
                         append_dict(directory_structure, current_dir, current_dir_list)
-                
+
                 if cmd_output[2] == '/':
                     current_dir = ['/']
                 elif cmd_output[2] == '..':
                     current_dir.pop()
                 else:
                     current_dir.append(cmd_output[2])
-                
+
                 list_dir = False
             if cmd_output[1] == 'ls':
                 list_dir = True
@@ -45,37 +44,40 @@ with open('python/07/07_example.txt', 'r') as file:
                 current_dir_list['files'] = []
                 current_dir_list['size'] = 0
             current_dir_list['files'].append((cmd_output[0], cmd_output[1]))
-            current_dir_list['size'] = current_dir_list['size'] + int(cmd_output[0])
+            current_dir_list['size'] += int(cmd_output[0])
     append_dict(directory_structure, current_dir, current_dir_list)
-            
-pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(directory_structure)
 
 size = {}
+flattened = flatdict.FlatDict(directory_structure, '.')
 
-def walk_dictionary(node):
-    for key, item in node.items():
-        temp_size = 0
-        if isinstance(item, dict):
-            print(item)
-            temp_size += item.get('size')
-            temp_size += walk_dictionary(item)
-    return temp_size
-            #if size < 100000 and size > 0 and key != 'files' and key != 'size':
-            #    accounted_for.append(key)
-            #    below_100000.append(size)
-
-def sum_elements(element):
-    if isinstance(element, dict):
-        walk_dictionary(element)
-        sum_elements(element)
-        
-size['/'] = walk_dictionary(directory_structure)
-sum_elements(directory_structure)
-print(size)            
+for key, value in flattened.items():
+    string_key = str(key).split('.')
+    if string_key[-1] == 'size':
+        for i, item in enumerate(string_key):
+            if item == 'size':
+                break
+            new_key = ''.join(map(str,string_key[:-(i+1)]))
+            if not size.get(new_key):
+                size[new_key] = 0
+            size[new_key] += value
 
 overall = 0
+over_size = []
+
+max_size = 70_000_000
+size_needed = 30_000_000
+current_available = max_size - size['/']
+directory_size_to_delete = size_needed - current_available
+
 for key, value in size.items():
-    if value < 100000:
+    # Part 1
+    if value < 100_000:
         overall += value
-print(f'ughhh: {overall}')
+
+    # Part 2
+    if value > directory_size_to_delete:
+        print(key, value)
+        over_size.append(value)
+
+print(f'Part 1: {overall}')
+print(f'Part 2: {min(over_size)}')
